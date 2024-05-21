cb1ba14bb7bf634096a07f0c1d17a2d3ad2f9d1e
ikorkmazov <korkmazovibra@yandex.ru>
2024-05-21 18:26:20 +0300

    2. use state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
 
+    const [likes, setLikes] = useState(5)
+
+    function inc() {
+       setLikes(likes+1)
+    }
+
+    function dec() {
+        setLikes(likes-1)
+    }
+
+    return (
+    <div className="App">
+        <h1>{likes}</h1>
+        <button onClick={inc}>Inc</button>
+        <button onClick={dec}>Dec</button>
     </div>
   );
 }
