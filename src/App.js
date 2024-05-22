0d053b420ee2122610e26dc84761a845e8ec060b
ikorkmazov <korkmazovibra@yandex.ru>
2024-05-22 17:14:43 +0300

    4. add post list component, post item, module css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 function App() {
 
-    // const [value, setValue] = useState("Text in input")
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JS', body: 'Description'},
+        {id: 2, title: 'JS1', body: 'Description'},
+        {id: 3, title: 'JS2', body: 'Description'}
+    ])
 
     return (
-    <div className="App">
-        <Counter/>
-        <ClassCounter/>
-        {/*<h1>{value}</h1>*/}
-        {/*<input*/}
-        {/*    type="text"*/}
-        {/*    value={value}*/}
-        {/*    onChange={event => setValue(event.target.value)}*/}
-        {/*/>*/}
-    </div>
-  );
+        <div className="App">
+            <PostForm/>
+            <PostList posts={posts} title={"JS"}/>
+        </div>
+    );
 }
 
 export default App;
